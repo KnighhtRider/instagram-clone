66dcd32e36b3a73e13485eee63b8e8e1a0ef4359
Vivek <vivekthakur6259@gmail.com>
2023-05-14 16:12:40 +0530

    :hammer: Feed (Stories, posts and Suggestions) UI

--- components/Feed.js
+++ components/Feed.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import Stories from './Stories'
+import Posts from './Posts'
+import MiniProfile from './MiniProfile'
+import Suggestions from './Suggestions'
 
 function Feed() {
 
   return (
-    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-7 xl:max-w-6xl mx-auto'>
 
-      <section className='col-span-2'>
-        {/* Stories */}
+      <section className='col-span-1'></section>
+
+      <section className='col-span-4 h-100 w-100'>
         <Stories />
-        {/* Post */}
+        <Posts />
       </section>
 
-      <section>
-        {/* Mini Profile */}
-        {/* Suggestions */}
+      <section className='hidden xl:inline-grid md:col-span-2'>
+        <div className='fixed top-25'>
+          <MiniProfile />
+          <Suggestions />
+        </div>
       </section>
 
     </main>
