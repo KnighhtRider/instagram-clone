93af9a1bd093b835f33bb797e013d3075eeb11ca
Vivek <vivekthakur6259@gmail.com>
2023-05-21 10:47:22 +0530

    To merge the branch

--- Backend/secrets/keys.js
+++ Backend/secrets/keys.js
@@ -1,4 +1,9 @@
 const mongoUrl = 'mongodb+srv://admin-vivek:P7p4ruU3qJUOTlff@instagram.etp0d8a.mongodb.net/?retryWrites=true&w=majority';
 
 
-module.exports = mongoUrl;
+module.exports = {
+  mongoUrl: 'mongodb+srv://admin-vivek:P7p4ruU3qJUOTlff@instagram.etp0d8a.mongodb.net/?retryWrites=true&w=majority',
+  Jwt_secret: 'hasldkfhdkd'
+}
+
+
