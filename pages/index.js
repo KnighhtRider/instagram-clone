b5b71f88862f2fdc6dbaef90e091c5853aad2fd7
Vivek <vivekthakur6259@gmail.com>
2023-11-03 16:00:42 +0530

    Local Storage Deleted

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,8 @@ import Header from "@/components/Header"
 import Head from "next/head"
 import Feed from "@/components/Feed"
 import Modal from "@/components/Modal"
-import { useRouter } from 'next/router';
-import { getTokenFromLocalStorage } from '../pages/utils/auth'
 
 export default function Home() {
-
-  const router = useRouter();
-  const token = getTokenFromLocalStorage();
 
   return (
     <div className='h-screen overflow-y-scroll scrollbar-hide'>
--- pages/utils/auth.js
+++ pages/utils/auth.js
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-const AuthPage = () => {
-  // Your authentication page logic
-  return <div>This is the authentication page.</div>;
-};
+// const AuthPage = () => {
+//   // Your authentication page logic
+//   return <div>This is the authentication page.</div>;
+// };
 
-export const getTokenFromLocalStorage = () => {
-  if (typeof window !== 'undefined') {
-    // Check if the window object is available (client-side)
-    const token = localStorage.getItem('jwt');
-    return token;
-  }
-  return null;
-};
+// export const getTokenFromLocalStorage = () => {
+//   if (typeof window !== 'undefined') {
+//     // Check if the window object is available (client-side)
+//     const token = localStorage.getItem('jwt');
+//     return token;
+//   }
+//   return null;
+// };
 
 
-export default AuthPage;
+// export default AuthPage;
